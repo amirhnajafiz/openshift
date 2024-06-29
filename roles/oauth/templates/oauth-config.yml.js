1a3c63eb5c217471b591336ee6096645349eeb07
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-29 10:23:38 +0330

    add: oauth tasks and tmps

---
+++ roles/oauth/templates/oauth-config.yml.js
@@ -0,0 +1,21 @@
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: keycloak
+    mappingMethod: claim
+    type: OpenID
+    openID:
+      clientID: keycloak-client-id
+      clientSecret:
+        name: keycloak-client-secret
+      claims:
+        preferredUsername:
+        - preferred_username
+        name:
+        - name
+        email:
+        - email
+      issuer: https://{{ keycloak_url }}/auth/realms/{{ keycloak_realm }}
