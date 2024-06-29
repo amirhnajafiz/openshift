5b238dca6151df6d25fee5ab3d48e023eb111a6f
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-29 10:12:57 +0330

    add: bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-ansible-playbook playbooks/openshift.yaml
+# Set variables
+INVENTORY_FILE="inventory/hosts"
+PLAYBOOK_FILE="playbook.yml"
+ANSIBLE_CFG_FILE="ansible.cfg"
+
+# Check if Ansible is installed
+if ! command -v ansible-playbook &> /dev/null
+then
+    echo "Ansible is not installed. Please install Ansible first."
+    exit 1
+fi
+
+# Run the Ansible playbook
+ansible-playbook -i $INVENTORY_FILE $PLAYBOOK_FILE --extra-vars "@/path/to/vars.yml"
+
+# Check the result of the ansible-playbook command
+if [ $? -ne 0 ]; then
+    echo "Ansible playbook execution failed."
+    exit 1
+else
+    echo "Ansible playbook executed successfully."
+fi
